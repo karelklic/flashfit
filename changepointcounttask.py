e296fbb1b8f100fc8e438105f4a53a185cd0ba87
Karel Klic <kklic@redhat.com>
2010-01-29 12:11:34 +0100

    Added new tasks (separate threads) for file loading and point reloading

---
+++ changepointcounttask.py
@@ -0,0 +1,47 @@
+from PyQt4 import QtCore, QtGui
+from data import Data
+
+class ChangePointCountTask(QtCore.QThread):
+    messageAdded = QtCore.pyqtSignal(QtCore.QString)
+    def __init__(self, pointCount, mainWindow, parent = None):
+        """
+        """
+        super(ChangePointCountTask, self).__init__(parent)
+        self.pointCount = pointCount
+        self.mainWindow = mainWindow
+        self.finished.connect(self.postRun)
+
+        # Set the maxPoints _now_, otherwise two threads are started.
+        self.mainWindow.data.maxPoints = self.pointCount
+
+        # Do not act on timeAxisLength changes when loading.
+        # Maybe a problem here because it's called from another thread.
+        #self.mainWindow.settings.timeAxisLength.valueChangeFinished.disconnect(self.mainWindow.scene.changeWidth)
+
+    def run(self):
+        """
+        The code in this method is run in another thread.
+        """
+        # Save fulllightvoltage pointer and fit absorbance pointer time, to be recovered after loading
+        fullLightVoltageTimes = self.mainWindow.data.fullLightVoltageTimes()
+        fitAbsorbanceTimes = self.mainWindow.data.fitAbsorbanceTimes()
+
+        self.messageAdded.emit("Copying %d points from loaded data..." % self.mainWindow.data.maxPoints)
+        self.mainWindow.data.copyFromOriginalData()
+
+        # Recover fulllightvoltage pointer and fit absorbance pointer times
+        self.mainWindow.data.setFullLightVoltageTimes(fullLightVoltageTimes, False)
+        self.mainWindow.data.setFitAbsorbanceTimes(fitAbsorbanceTimes, False)
+
+        self.messageAdded.emit("Computing absorbance...")
+        self.mainWindow.data.recalculateAbsorbances()
+        self.messageAdded.emit("Fitting absorbance")
+        self.mainWindow.data.fitAbsorbances(self.messageAdded.emit)
+
+    def postRun(self):
+        """
+        The code in this method is run in GUI thread.
+        """
+        self.mainWindow.scene.updateFromData()
+        # Connect the settings signal back to the slot.
+        #self.mainWindow.settings.timeAxisLength.valueChangeFinished.connect(self.mainWindow.scene.changeWidth)
---
+++ loadfiletask.py
@@ -0,0 +1,49 @@
+import csv
+from PyQt4 import QtCore, QtGui
+from data import Data
+
+class LoadFileTask(QtCore.QThread):
+    messageAdded = QtCore.pyqtSignal(QtCore.QString)
+    def __init__(self, name, mainWindow, parent = None):
+        """
+        Parameters
+        name: name of the opened file.
+        """
+        super(LoadFileTask, self).__init__(parent)
+        self.name = name
+        self.mainWindow = mainWindow
+        self.finished.connect(self.postRun)
+
+    def run(self):
+        """
+        The code in this method is run in another thread.
+        """
+        # Load data
+        reader = csv.reader(open(self.name))
+        try:
+            self.mainWindow.data.originalData.readFromCsvReader(reader, self.messageAdded.emit)
+        except (StopIteration, csv.Error):
+            QtGui.QMessageBox.critical(self, "Error while loading file", \
+                                           "Error occured when loading " + name)
+            return
+
+        self.mainWindow.data.maxPoints = Data.DEFAULT_USED_POINTS_COUNT
+        self.messageAdded.emit("Copying %d points from loaded data..." % self.mainWindow.data.maxPoints)
+        self.mainWindow.data.copyFromOriginalData()
+        self.messageAdded.emit("Computing absorbance...")
+        self.mainWindow.data.guessFullLightVoltagePointerValue() # sets fullLightVoltage
+        self.mainWindow.data.recalculateAbsorbances()
+        self.messageAdded.emit("Fitting absorbance")
+        self.mainWindow.data.guessFitAbsorbanceTimePointer()
+        self.mainWindow.data.fitAbsorbances(self.messageAdded.emit)
+
+    def postRun(self):
+        """
+        The code in this method is run in GUI thread.
+        """
+        # Refresh GUI
+        self.mainWindow.scene.updateFromData()
+        self.mainWindow.setWindowTitle(QtCore.QFileInfo(self.name).fileName() + " - flashfit")
+        self.mainWindow.settings.onDataLoaded(self.mainWindow.data)
+        # Update Recent files in the Main Menu
+        self.mainWindow.menuBar().addRecentFile(self.name)        
